d592bdb1170c6a11251cefbe743485340a117aff
Stefan Kendall <stefankendall@gmail.com>
2014-12-07 16:10:24 -0500

    Active timer can be stopped. Need a set cell for time.

--- SmarterWorkouts/TimerActiveCell.m
+++ SmarterWorkouts/TimerActiveCell.m
@@ -1,5 +1,8 @@
 #import "TimerActiveCell.h"
 #import "Activity.h"
+#import "Set.h"
+#import "NSManagedObject+MagicalRecord.h"
+#import "ActivityFormDelegate.h"
 
 const NSString *TIMER_ACTIVE_ACTIVITY = @"activetimer";
 
@@ -8,6 +11,13 @@ const NSString *TIMER_ACTIVE_ACTIVITY = @"activetimer";
 - (void)setActivity:(Activity *)activity {
     [super setActivity:activity];
     [self.activityName setText:activity.name];
+}
+
+- (IBAction)doneButtonTapped:(id)sender {
+    Set *set = [Set MR_createEntity];
+    set.activity = self.selectedSet ? self.selectedSet.activity : self.activity.name;
+    set.duration = @(2);
+    [self.activityFormDelegate formFinished:@[set]];
 }
 
 @end
