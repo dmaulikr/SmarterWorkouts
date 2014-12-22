d4c44c10076138fc63ec2a41da84dab992c9b862
Stefan Kendall <stefankendall@gmail.com>
2014-12-21 19:49:47 -0500

    Reading quotes from file.

--- SmarterWorkouts/QuoteViewController.m
+++ SmarterWorkouts/QuoteViewController.m
@@ -2,4 +2,19 @@
 
 @implementation QuoteViewController
 
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self useRandomQuote];
+}
+
+- (void)useRandomQuote {
+    NSArray *quotes = [NSJSONSerialization JSONObjectWithData:[NSData dataWithContentsOfFile:
+                    [[NSBundle mainBundle] pathForResource:@"quotes" ofType:@"json"]]
+                                                      options:(NSJSONReadingOptions) kNilOptions error:nil];
+
+    NSDictionary *quote = quotes[arc4random_uniform([quotes count])];
+    [self.author setText:quote[@"author"]];
+    [self.quote setText:quote[@"quote"]];
+}
+
 @end
