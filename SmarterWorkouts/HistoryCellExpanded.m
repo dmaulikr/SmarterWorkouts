470f677808197843f6f605af9df21b807bf239fe
Stefan Kendall <stefankendall@gmail.com>
2014-12-23 13:41:21 -0500

    Fixed edit button background color

--- SmarterWorkouts/HistoryCellExpanded.m
+++ SmarterWorkouts/HistoryCellExpanded.m
@@ -9,7 +9,7 @@
 - (void)awakeFromNib {
     [super awakeFromNib];
     [editButton setBackgroundImage:
-                    [UIImage imageWithColor:[UIColor colorWithRed:213 / 255.0f green:116 / 255.0f blue:0 / 255.0f alpha:1.0f]]
+                    [UIImage imageWithColor:[UIColor colorWithRed:155 / 255.0f green:84 / 255.0f blue:0 / 255.0f alpha:1.0f]]
                           forState:UIControlStateHighlighted];
 }
 
