2d031c414b4386fe75e7fd5f3aa9ff7908ee1a4f
Stefan Kendall <stefankendall@gmail.com>
2014-12-24 11:46:58 -0500

    Weight form now has set cell. Need cell for duration.

--- SmarterWorkouts/SmallSetCell.m
+++ SmarterWorkouts/SmallSetCell.m
@@ -1,10 +1,16 @@
 #import "SmallSetCell.h"
 #import "Set.h"
 
-@implementation SmallSetCell
+@implementation SmallSetCell {
+    __weak IBOutlet UILabel *activityLabel;
+    __weak IBOutlet UILabel *weightLabel;
+    __weak IBOutlet UILabel *repsLabel;
+}
 
 - (void)setSet:(Set *)set {
-
+    [activityLabel setText:set.activity];
+    [weightLabel setText:[NSString stringWithFormat:@"%@%@", set.weight, set.units]];
+    [repsLabel setText:[NSString stringWithFormat:@"%dx", [set.reps intValue]]];
 }
 
 @end
